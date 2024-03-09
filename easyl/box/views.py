d2f37d6cc3a37d9aed031e3435887aaa81c30d3b
alexmais <alexlove9596@gmail.com>
2024-03-09 11:45:52 +0200

    refact

--- easyl/box/views.py
+++ easyl/box/views.py
@@ -1,12 +1,6 @@
-from django.shortcuts import render
 from .models import *
 from .serializers import *
-from rest_framework import permissions, viewsets
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 
 
@@ -26,25 +20,6 @@ class ClientApiView(APIView):
 
 
 
-# @csrf_exempt
-# def client_by_pk(request, pk):
-#     try:
-#         client = Client.objects.get(pk=pk)
-#     except Client.DoesNotExist:
-#         return HttpResponse(status=404)
-#     if request.method == 'GET':
-#         serializer = ClientSerializer(client)
-#         return JsonResponse(serializer.data)
-  
-@csrf_exempt  
-def add_client(request):
- 
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ClientSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+
 
 
